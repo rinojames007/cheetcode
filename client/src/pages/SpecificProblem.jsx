77dd0c0f648241b4ab2704edb2462bd0743b57a0
rinojames007 <rinojames.krl@gmail.com>
2023-05-21 17:41:30 +0530

    initial commit

--- client/src/pages/SpecificProblem.jsx
+++ client/src/pages/SpecificProblem.jsx
@@ -1,14 +1,26 @@
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import {cpp} from "@codemirror/lang-cpp";
+
 function SpecificProblem(){
+    const onChange = React.useCallback((value, viewUpdate) => {
+        console.log('value:', value);
+    }, []);
     return (
-        <div className={"container w-11/12 border-red-400 border-2 h-screen flex"}>
-            <div className={"w-screen border-red-400 border-2 h-1/2 flex-col items-center text-center"}>
-                <h1>799. binary search</h1>
-                <p>Description</p>
-                <br/>
-                <p>Input : 2</p>
-                <p>Output : 1</p>
+        <div className={"w-10/12 h-screen mx-auto flex"}>
+            <div className={"w-full h-3/4 flex-col text-center my-auto"}>
+                <h1>1. Find first and last occurrence</h1>
             </div>
-            <div className={"container w-screen border-red-400 border-2 h-1/2"}></div>
+            <div className={"w-full h-full border-2 flex-col text-center my-auto"}>
+                <h4>Write your code here:</h4>
+                <CodeMirror
+                    value=""
+                    height="90vh"
+                    extensions={[cpp()]}
+                    onChange={onChange}
+                />
+            </div>
         </div>
     );
 };
